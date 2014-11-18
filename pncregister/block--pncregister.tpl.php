c93228385c07c34f76a62e2be666abb360e2acaa
purencool <purencool@gmail.com>
2014-11-19 09:29:50 +1100

    Adding new modules

---
+++ pncregister/block--pncregister.tpl.php
@@ -0,0 +1,22 @@
+<div class="lightboxOverlay"`>
+</div>
+ <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?> lightboxContainer"<?php print $attributes; ?>>
+  <?php print render($title_prefix); ?>
+  <?php if ($subject): ?>
+     <h2<?php print $title_attributes; ?>>Registration</h2>
+  <?php endif;?>
+  <?php print render($title_suffix); ?>
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php //print drupal_render_children($content); ?>
+     <form class="user-info-from-cookie" enctype="multipart/form-data" action="/" method="post" id="user-register-form" accept-charset="UTF-8">
+       <?php  print drupal_render($content['account']['name']); ?>
+       <?php print drupal_render($content['field_last_name']); ?>
+       <?php  print drupal_render($content['account']['mail']); ?>
+       <?php print drupal_render($content['field_contact_phone']); ?>
+       <?php  print drupal_render($content['form_build_id']); ?>
+       <?php  print drupal_render($content['form_id']); ?>
+       <?php  print drupal_render($content['actions']); ?>
+     </form>
+   </div>
+   <div class="clear-div">
+ </div>
